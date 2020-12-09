79382a775ee1090ff6b0969d20123bd534a40453
nmhanson339 <normh2048@gmail.com>
2020-12-09 07:01:38 -0500

    Define functions for form submission

--- js/main.js
+++ js/main.js
@@ -1,15 +1,25 @@
-// VARIABLES
+// HTML ELEMENTS
 let header = document.querySelector(".page-header");
 let headerNav = document.querySelector(".header-nav");
 let headerNavBtn = document.querySelector(".header-nav-btn");
+let contactForm = document.getElementById("portfolio-contact");
+let contactFormBtn = document.getElementById("GET-submission");
+let contactFormStatus = document.getElementById("contact-form-status");
 
-// EVENT LISTENER
+// EVENT LISTENERS
 headerNavBtn.addEventListener(
   "click",
   toggleAccordionMenu.bind(headerNavBtn, headerNav, header)
 );
 
 headerNavBtn.addEventListener("click", toggleMenuIcon.bind(headerNavBtn));
+
+// handle the form submission event
+contactForm.addEventListener("submit", function (ev) {
+  ev.preventDefault();
+  var data = new FormData(contactForm);
+  ajax(contactForm.method, contactForm.action, data, success, error);
+});
 
 // FUNCTIONS
 function toggleMenuIcon() {
@@ -35,4 +45,35 @@ function toggleAccordionMenu(menu, menuParent) {
     menu.style.transition = `transform 0.8s ease-in-out`;
     menuParent.style.transition = `height 0.8s ease-in-out`;
   }
+}
+
+// - - - - - FORM FUNCTIONS - - - - -
+// Courtesy of FormSpree.com
+
+// Success and Error functions for after the form is submitted
+function success() {
+  contactForm.reset();
+  contactFormBtn.style = "display: none "; //!!!!!!!!Change to visibility: hidden
+  contactFormStatus.innerHTML = "Thanks!";
+}
+
+function error() {
+  contactFormStatus.innerHTML = "Oops! There was a problem.";
+}
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
 }
